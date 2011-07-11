9548d689018460ea94b8462e2f8fa232992d342f
joe di castro <joe@joedicastro.com>
2011-07-11 23:01:52 +0200

    Improved rsync_fabfile: Added a backup option and control for multiple hosts.

--- src/rsync_fabfile.py
+++ src/rsync_fabfile.py
@@ -24,40 +24,137 @@
 
 __author__ = "joe di castro <joe@joedicastro.com>"
 __license__ = "GNU General Public License version 3"
-__date__ = "06/07/2011"
-__version__ = "0.1"
+__date__ = "11/07/2011"
+__version__ = "0.2"
 
-
+import os
+import glob
+import tarfile
+import time
+from get_size import get_size as _get_size
+from get_size import best_unit_size as _best_unit_size
 from logger import Logger as _logger
 from notify import notify as _notify
 from fabric.api import env, local
 
+LOG = _logger()
+
+#===============================================================================
+# RSYNC HOSTS
+#===============================================================================
+
+# Your default host. No need any more if only wants a host.
 env.host_string = "username@host"
-REMOTE_PATH = "/your/remote/path"
-LOCAL_PATH = "/your/local/path"
+env.remote = "/your/remote/path"
+env.local = "/your/local/path"
+
+# If wants to use various hosts, then define the previous variables like this, 
+# one function per host. 
+def host_1():
+    """Host variables for host_1."""
+    global env
+    env.host_string = "username@host_1"
+    env.remote = "/your/remote/path/in/host_1"
+    env.local = "/your/local/path/for/host_1"
+
+def host_2():
+    """Host variables for host_2."""
+    global env
+    env.host_string = "username@host_2"
+    env.remote = "/your/remote/path/in/host_2"
+    env.local = "/your/local/path/for/host_2"
+
+# ...
+#
+# def host_n():
+#     """Host variables for host_n."""
+#     global env
+#     env.host_string = "username@host_n"
+#     env.remote = "/your/remote/path/in/host_n"
+#     env.local = "/your/local/path/for/host_n"
+
+#===============================================================================
+# END RSYNC HOSTS
+#===============================================================================
+
+def _log_start():
+    """Create the Start time info block for the log."""
+    # Init the log for multiple hosts. Do not repeat the previous logs.
+    if LOG.get():
+        LOG.__init__()
+    LOG.time("Start time")
+
+def _log_end():
+    """Create the End time info block and send & write the log."""
+    _notify("Rsync", "Ended" , "ok")
+    LOG.time("End time")
+    LOG.free(os.linesep * 2)
+    LOG.write(True)
+    LOG.send("Fabric Rsync")
 
 def _rsync(source, target, delete):
     """Process the _rsync command."""
-    log = _logger()
-    log.header("Fabric Rsync\nhttp://code.joedicastro.com/python-recipes",
+    _log_start()
+    LOG.header("Fabric Rsync\nhttp://code.joedicastro.com/python-recipes",
                "Syncing {0} to {1}".format(source, target))
-    log.time("Start time")
     _notify("Rsync", "Start syncing {0} to {1}".format(source, target), "info")
-    output = local("rsync -pthrvz {0}/ {1} {2}".
-                   format(source, target, "--delete" if delete == "yes" else ""),
-                   capture=True)
-    _notify("Rsync", "Finished", "ok")
-    log.list("Output", output)
-    if output.failed:
-        log.list("Error", output.stderr)
-    log.time("End time")
-    log.send("Fabric Rsync")
+    out = local("rsync -pthrvz {0}/ {1} {2}".
+                format(source, target, "--delete" if delete == "yes" else ""),
+                capture=True)
+    _notify("Rsync", "Finished synchronization", "ok")
+    LOG.list("Rsync Output", out)
+    if out.failed:
+        LOG.list("Error", out.stderr)
 
-def up(dlt='yes'):
+def _compress(path):
+    """Compress a local directory into a gz file.
+
+    Creates a file for each weekday, an removes the old files if exists"""
+    dir2gz = os.path.basename(path)
+    old_gzs = glob.glob('{0}*{1}.tar.gz'.format(dir2gz, time.strftime('%a')))
+    gz_name = "{0}_{1}.tar.gz".format(dir2gz, time.strftime('%d%b%Y_%H:%M_%a'))
+    gz_file = tarfile.open(gz_name, "w:gz")
+    gz_file.add(path, arcname=dir2gz)
+    gz_file.close()
+    output = os.linesep.join(['Created file:', '', os.path.join(os.getcwd(),
+                                                                gz_name)])
+    for old_gz in old_gzs:
+        os.remove(old_gz)
+        output += os.linesep.join([os.linesep, 'Deleted old file:', '', old_gz])
+    return output
+
+def _archive():
+    """Archive the local directory in a gz file for each weekday."""
+    _notify('Compressing folder...', 'info')
+    LOG.list('Rotate compressed copies', _compress(env.local))
+    _notify("Rsync", "Finished compression", "Ok")
+
+def _get_diskspace():
+    """Get the disk space used by the local directory and archives."""
+    gz_size = sum([_get_size(gz) for gz in glob.glob('{0}*.gz'.
+                                                     format(env.local))])
+    log_size = _get_size(LOG.filename) if os.path.exists(LOG.filename) else 0
+    local_size = _get_size(env.local)
+    size = _best_unit_size(local_size + gz_size + log_size)
+    LOG.block('Disk space used', '{0:>76.2f} {1}'.format(size['s'], size['u']))
+
+def up(server=None, dlt='yes'):
     """Sync from local to remote."""
-    _rsync(LOCAL_PATH, ":".join([env.host_string, REMOTE_PATH]), dlt)
+    globals()[server]() if server else None
+    _rsync(env.local, ":".join([env.host_string, env.remote]), dlt)
+    _log_end()
 
-def down(dlt='yes'):
+def down(server=None, dlt='yes', archive=False):
     """Sync from remote to local."""
-    _rsync(":".join([env.host_string, REMOTE_PATH]), LOCAL_PATH, dlt)
+    globals()[server]() if server else None
+    _rsync(":".join([env.host_string, env.remote]), env.local, dlt)
+    if not archive:
+        _log_end()
+
+def backup(server=None):
+    """Sync from remote to local and archive the directory."""
+    down(server, archive=True)
+    _archive()
+    _get_diskspace()
+    _log_end()
 
