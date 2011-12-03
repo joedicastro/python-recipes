17dc1cf33fdda510af1a7979414f36b58b9dbea7
joe di castro <joe@joedicastro.com>
2011-12-03 20:51:48 +0100

    djvu2pdf: more polished version

--- src/djvu2pdf.py
+++ src/djvu2pdf.py
@@ -1,93 +1,95 @@
 #!/usr/bin/env python
-# -*- coding: <utf8> -*-
+# -*- coding: utf8 -*-
 
 """
-    djvu2pdf.py:
+    djvu2pdf.py: Converts a .djvu file into a .pdf file
 """
 
-#===============================================================================
-# This Script does...
-#===============================================================================
+#==============================================================================
+# This Script does exactly as the description above says.
+#==============================================================================
 
-#===============================================================================
-#       Copyright 2009 joe di castro <joe@joedicastro.com>
-#       
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
-#===============================================================================
+#==============================================================================
+#    Copyright 2011 joe di castro <joe@joedicastro.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#==============================================================================
 
-__author__ = "joe di castro - joe@joedicastro.com"
-__license__ = "GNU General Public License version 2"
-__date__ = "16/06/2009"
-__version__ = "0.1"
+__author__ = "joe di castro <joe@joedicastro.com>"
+__license__ = "GNU General Public License version 3"
+__date__ = "03/12/2011"
+__version__ = "0.2"
 
-try: 
+try:
     import sys
     import os
-    import optparse
-    import re
-    import subprocess
-
+    from argparse import ArgumentParser
+    from subprocess import Popen, PIPE
 except ImportError:
-    print ("""An error found importing one or more modules:
-    \n{0}
-    \nYou need to install this module\nQuitting...""").format(sys.exc_info()[1])
+    # Checks the installation of the necessary python modules
+    print((os.linesep * 2).join(["An error found importing one module:",
+    str(sys.exc_info()[1]), "You need to install it", "Stopping..."]))
     sys.exit(-2)
-    
-def check_execs():
-    """Check if the programs are installed """
-    msg = 'The ddjvu and tiff2pdf programs are necessary to run this script.'
-    try:
-        pipe = subprocess.PIPE
-        subprocess.Popen(['ddjvu', '--help'], stdout=pipe, stderr=pipe)
-        subprocess.Popen(['tiff2pdf', '--help'], stdout=pipe, stderr=pipe)
-    except OSError:
-        sys.exit(msg)
+
+
+def check_execs(*progs):
+    """Check if the programs are installed, if not exit and report."""
+    for prog in progs:
+        try:
+            Popen([prog, '--help'], stdout=PIPE, stderr=PIPE)
+        except OSError:
+            msg = 'The {0} program is necessary to run the script'.format(prog)
+            sys.exit(msg)
     return
 
 
-def options():
-    """Defines the command line arguments and options for the script"""
-    usage = """usage: %prog file.djvu"""
-    desc = "Converts a djvu file into a pdf file"
-    parser = optparse.OptionParser(usage=usage, version="%prog " + __version__,
-                                   description=desc)
+def arguments():
+    """Defines the command line arguments for the script."""
+    main_desc = """Converts a djvu file into a pdf file"""
+
+    parser = ArgumentParser(description=main_desc)
+    parser.add_argument("file", help="The djvu file")
+    parser.add_argument("-v", "--version", action="version",
+                        version="%(prog)s {0}".format(__version__),
+                        help="show program's version number and exit")
     return parser
 
 
-def main():
-    (opts, args) = options().parse_args()
-    output = []
-    if not args:
-        sys.exit()
-    else:
-        djvu_file = args[0]
-        filename = re.findall("(.+).djvu", djvu_file)[0]
-    command_tif = ['ddjvu', '-format=tiff', '{0}.djvu'.format(filename),
-                   '{0}.tif'.format(filename)]
-    to_tiff = subprocess.Popen(command_tif, stdout=subprocess.PIPE)
+def main(output=[]):
+    args = arguments().parse_args()
+    djvu_file = args.file
+
+    if not os.path.exists(djvu_file):
+        sys.exit("ERROR: cannot open '{0}' (No such file)".format(djvu_file))
+
+    djvu_filename = djvu_file.split(".djvu")[0]
+    tiff = '{0}.tif'.format(djvu_filename)
+    pdf = '{0}.pdf'.format(djvu_filename)
+
+    to_tiff = Popen(['ddjvu', '-format=tiff', djvu_file, tiff], stdout=PIPE)
     output.append(to_tiff.stdout.readlines())
-    command_pdf = ['tiff2pdf', '-j', '-o', '{0}.pdf'.format(filename),
-                    '{0}.tif'.format(filename)]
-    to_pdf = subprocess.Popen(command_pdf, stdout=subprocess.PIPE)
-    output.append(to_pdf.stdout.readlines())
+
+    if os.path.exists(tiff):
+        to_pdf = Popen(['tiff2pdf', '-j', '-o', pdf, tiff], stdout=PIPE)
+        output.append(to_pdf.stdout.readlines())
+        os.remove(tiff)
+
     for line in output:
         if line:
-            print line
-    os.remove('{0}.tif'.format(filename))
+            print(line)
+
 
 if __name__ == "__main__":
-    check_execs() # Check first if programs are installed
+    check_execs('ddjvu', 'tiff2pdf')
     main()
