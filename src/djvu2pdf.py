7d89683443abd37c9d93ac02457d75b52e0fe754
joe di castro <joe@joedicastro.com>
2011-12-03 21:30:43 +0100

    djvu2pdf: Modified for batch operations & fixed some errors

--- src/djvu2pdf.py
+++ src/djvu2pdf.py
@@ -29,7 +29,7 @@
 __author__ = "joe di castro <joe@joedicastro.com>"
 __license__ = "GNU General Public License version 3"
 __date__ = "03/12/2011"
-__version__ = "0.2"
+__version__ = "0.3"
 
 try:
     import sys
@@ -59,35 +59,33 @@ def arguments():
     main_desc = """Converts a djvu file into a pdf file"""
 
     parser = ArgumentParser(description=main_desc)
-    parser.add_argument("file", help="The djvu file")
+    parser.add_argument("file", nargs="+", help="The djvu file")
     parser.add_argument("-v", "--version", action="version",
                         version="%(prog)s {0}".format(__version__),
                         help="show program's version number and exit")
     return parser
 
 
-def main(output=[]):
+def process(command, fname):
+    errors = Popen(command, stderr=PIPE).stderr.readlines()
+    for line in errors:
+        print("{0}: {1}".format(fname.upper(), line.rstrip(os.linesep)))
+
+def main():
     args = arguments().parse_args()
-    djvu_file = args.file
+    djvu_files = args.file
 
-    if not os.path.exists(djvu_file):
-        sys.exit("ERROR: cannot open '{0}' (No such file)".format(djvu_file))
-
-    djvu_filename = djvu_file.split(".djvu")[0]
-    tiff = '{0}.tif'.format(djvu_filename)
-    pdf = '{0}.pdf'.format(djvu_filename)
-
-    to_tiff = Popen(['ddjvu', '-format=tiff', djvu_file, tiff], stdout=PIPE)
-    output.append(to_tiff.stdout.readlines())
-
-    if os.path.exists(tiff):
-        to_pdf = Popen(['tiff2pdf', '-j', '-o', pdf, tiff], stdout=PIPE)
-        output.append(to_pdf.stdout.readlines())
-        os.remove(tiff)
-
-    for line in output:
-        if line:
-            print(line)
+    for djvu in djvu_files:
+        if not os.path.exists(djvu):
+            print("ERROR: cannot open '{0}' (No such file)".format(djvu))
+        else:
+            djvu_filename = djvu.split(".djvu")[0]
+            tiff = '{0}.tif'.format(djvu_filename)
+            pdf = '{0}.pdf'.format(djvu_filename)
+            process(['ddjvu', '-format=tiff', djvu, tiff], tiff)
+            if os.path.exists(tiff):
+                process(['tiff2pdf', '-j', '-o', pdf, tiff], pdf)
+                os.remove(tiff)
 
 
 if __name__ == "__main__":
